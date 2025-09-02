299909b141c4f657f17499b7a954391f08a7cfe6
CraftyNuker <gallasicnoah@gmail.com>
2025-09-02 08:11:54 -0600

    chore(pages): add .nojekyll and enable output export

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Use static export output for GitHub Pages
+  output: 'export',
 }
 
 module.exports = nextConfig
